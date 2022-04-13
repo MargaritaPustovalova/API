5e67d20941535ea7c7dd67c99b643afdb5166623
MargaritaPustovalova <71708472+MargaritaPustovalova@users.noreply.github.com>
2022-04-13 19:30:32 +0300

    Add files via upload

---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+//    private class Birthday{
+// public String date;
+// public String month;
+// public String year;
+// }
+}
